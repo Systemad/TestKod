e0fedefc8b853b581c6a2accfc3cce0c33e979ca
systemad <dannepuke@gmail.com>
2020-04-08 10:02:31 +0200

    initial

---
+++ src/Chatroom/ReadThread.java
@@ -0,0 +1,70 @@
+package Chatroom;
+import java.awt.desktop.SystemEventListener;
+import java.io.IOException;
+import java.net.*;
+
+/*
+public class chatroomReceive implements Runnable {
+
+    public chatroomReceive(String messageFrom) throws SocketException, IOException{
+        int minPort = 55555;
+        String ip = "224.0.0.1";
+        InetAddress iadr = InetAddress.getByName(ip);
+        MulticastSocket socket = new MulticastSocket(minPort);
+
+
+        socket.joinGroup(iadr);
+        byte[] data = new byte[256];
+
+        while(true){
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+            socket.receive(packet);
+            String message = new String(packet.getData(), 0, packet.getLength());
+            //System.out.println(message);
+        }
+    }
+
+    @Override
+    public void run(){
+        while(!Thread.interrupted()){
+            try{
+                Thread.sleep(5000);
+            }
+            catch(InterruptedException e){
+                break;
+            }
+        }
+    }
+}
+*/
+
+public class ReadThread implements Runnable {
+    private MulticastSocket socket;
+    private  InetAddress adr;
+    private int port;
+    private String ip;
+
+    public ReadThread(MulticastSocket socket, InetAddress adr, int port) {
+        this.socket = socket;
+        this.adr = adr;
+        this.port = port;
+    }
+
+    @Override
+    public void run(){
+        while(!Thread.interrupted()){
+            byte[] data = new byte[256];
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+            String message;
+            try{
+                socket.receive(packet);
+                message = new String(packet.getData(), 0, packet.getLength());
+                //System.out.print(message);
+            }
+            catch(IOException e){
+                System.out.print("Connection Closed");
+                break;
+            }
+        }
+    }
+}
--- src/Chatroom/chatroomSend.java
+++ src/Chatroom/chatroomSend.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.net.*;
 import javax.swing.*;
 
-
+/*
 public class chatroomSend extends JFrame implements ActionListener {
 
     JPanel panel = new JPanel();
@@ -46,7 +46,7 @@ public class chatroomSend extends JFrame implements ActionListener {
         this.setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        chatroomReceive m = new chatroomReceive(messageFrom, mSocket);
+        chatroomReceive m = new chatroomReceive(messageFrom);
         Thread t = new Thread(m);
         t.start();
 
@@ -67,11 +67,83 @@ public class chatroomSend extends JFrame implements ActionListener {
         //textArea.setText(nickname + ": " + message);
         text.setText("");
         textArea.setText(messageFrom);
+        //System.out.println(messageFrom);
 
     }
 
     public static void main(String[] args) throws UnknownHostException, SocketException, IOException, InterruptedException{
         chatroomSend ts = new chatroomSend();
+
+    }
+}
+*/
+
+public class chatroomSend extends JFrame implements ActionListener {
+
+    JPanel panel = new JPanel();
+    JTextField text = new JTextField(10);
+    JTextArea textArea = new JTextArea(30, 20);
+
+
+    JButton button = new JButton("Send");
+    String nickname;
+    String message = "";
+    String dataToSend = "";
+
+    int toPort = 55555;
+    String group = "224.0.0.1";
+    InetAddress toAdr = InetAddress.getByName(group);
+    MulticastSocket mSocket = new MulticastSocket();
+
+
+    public chatroomSend()throws UnknownHostException, SocketException, IOException, InterruptedException{
+
+        nickname = JOptionPane.showInputDialog(null, "Nickname: ");
+        if (nickname == null || nickname.length() == 0){
+            System.exit(0);
+        }
+        this.add(panel);
+        panel.add(text);
+        panel.add(button);
+        panel.add(textArea);
+
+        button.addActionListener(this);
+        text.addActionListener(this);
+
+        this.pack();
+        this.setLocation(800, 300);
+        this.setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        mSocket.joinGroup(toAdr);
+        ReadThread m = new ReadThread(mSocket, toAdr, toPort);
+        Thread t = new Thread(m);
+        t.start();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        message = text.getText();
+        dataToSend = nickname+": "+message;
+        byte[] data = dataToSend.getBytes();
+        DatagramPacket packet = new DatagramPacket(data, data.length, toAdr, toPort);
+        try{
+            mSocket.send(packet);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        //textArea.setText(nickname + ": " + message);
+        text.setText("");
+        //System.out.println(messageFrom);
+
+
+    }
+
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException, InterruptedException{
+        chatroomSend ts = new chatroomSend();
+
 
     }
 }
